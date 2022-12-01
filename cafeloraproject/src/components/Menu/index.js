bf65047dde0958b00bdc1726d19ba08760260c6a
Tereza Smutna <terka.kop@seznam.cz>
2022-12-01 14:05:19 +0100

    stahnuti napoju z API

--- cafeloraproject/src/components/Menu/index.js
+++ cafeloraproject/src/components/Menu/index.js
@@ -2,7 +2,7 @@ import '../Menu/style.css';
 import { Drink } from './components/Drink/index.js'
 
 export const Menu = (props) => {
-  const { } = props;
+  const { drinks = [] } = props;
 
   const element = document.createElement('section');
   element.classList.add('menu');
@@ -23,25 +23,24 @@ export const Menu = (props) => {
   `;
 
 
-  const drinkList = element.querySelector('.drinks-list');
-  drinkList.append(
-    Drink({
-      id: 'romano',
-      name: 'Romano',
-      ordered: false,
-      image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-      layers: [
-        {
-          color: '#fbdf5b',
-          label: 'citrón',
-        },
-        {
-          color: '#613916',
-          label: 'espresso',
-        },
-      ]
-    }),
-  );
+  if (drinks.length === 0) {
+    fetch('https://apps.kodim.cz/daweb/cafelora/api/me/drinks', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Email tereza@seznam.com',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        element.replaceWith(Menu({
+          drinks: data.results,
+        }));
+      });
+
+  } else {
+    const drinkList = element.querySelector('.drinks-list');
+    drinkList.append(...drinks.map((drink) => Drink(drink)));
+  }
 
   return element;
 };
